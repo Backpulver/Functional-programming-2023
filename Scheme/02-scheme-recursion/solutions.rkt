43915fd6c1d172db11db59dd9abde4c9736afe5c
Yoan Hristov <32713575+Backpulver@users.noreply.github.com>
2023-10-31 01:51:45 +0200

    Updates on solutions 2

--- Scheme/02-scheme-recursion/solutions.rkt
+++ Scheme/02-scheme-recursion/solutions.rkt
@@ -4,4 +4,27 @@
         (+ sum (remainder n 10))
         (itersum (+ sum (remainder n 10)) (quotient n 10))))
   (itersum 0 n))
-      
+
+(define (count-divisors n)
+  (define (iter count i n)
+    (cond
+      ((= 0 n) 0)
+      ((> i n) count)
+      ((= 0 (remainder n i)) (iter (+ count 1) (+ i 1) n))
+      (else (iter count (+ i 1) n))))
+  (iter 0 1 n))
+
+(define (prime? n)
+  (define (iter dividers i n)
+    (cond
+      ((> dividers 2) #f)
+      ((or (= 0 n) (= 1 n)) #f)
+      ((<= i n)
+         (if (= 0 (remainder n i))
+            (iter (+ dividers 1) (+ i 1) n)
+            (iter dividers (+ i 1) n)))
+      (else #t)))
+  (iter 0 1 n))
+
+
+        
